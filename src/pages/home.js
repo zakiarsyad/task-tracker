e1d79c7488eeaa2a84cbc6d6e655debb62d3509d
Zaki Arsyad <zakiarsyad@gmail.com>
2024-09-02 20:21:51 +0700

    feat: add pages

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Container, Heading } from '@chakra-ui/react'
+
+export const Home = () => {
+    return (
+        <Container maxW="container.md">
+            <Heading textAlign="center" color="teal.600">Task Tracker</Heading>
+
+        </Container>
+    )
+}
